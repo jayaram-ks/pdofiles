0ac15566f93d17217e8c65d7acad0991ebcd7d81
Jayaram <jay@ks.com>
2017-01-15 23:20:21 +0400

    user class modified

--- manageuser.php
+++ manageuser.php
@@ -9,6 +9,17 @@ class ManageUsers()
 		$this->link = $dbconnection ->connect();
 		return $this link;
 	}
+	public function RegisterUsers($username,$password,$ip_address,$reg_date)
+	{
+		$query = $this->link->prepare("INSERT INTO users (username,password,ip_address,reg_date,reg_time) VALUES(?,?,?,?,?)");
+		$values = array($username,$password,$ip_address,$reg_date);
+		$query->execute($values);
+		$counts = $query->rowCount(); 
+	}
 
 }
+
+$users = new ManageUsers();
+$users->RegisterUsers('Jayaram','123','1.1.1.1','2015-03-11','12:00');
+
 ?>
